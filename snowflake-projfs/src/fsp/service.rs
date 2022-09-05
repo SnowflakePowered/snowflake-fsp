7542605559f8fbb319a61869569d03388ba303bc
chyyran <ronny@ronnychan.ca>
2022-09-04 23:47:22 -0400

    test: fix paths

--- snowflake-projfs/src/fsp/service.rs
+++ snowflake-projfs/src/fsp/service.rs
@@ -5,12 +5,12 @@ use snowflake_projfs_common::projections::parse_projection;
 #[inline]
 pub fn svc_start(args: Args) -> anyhow::Result<SnowflakeProjFs> {
     let projection = "
-f(/hello.world|C:\test.txt|r);
+f(/hello.world|C:\\test.txt|r);
 d(/directory|);
 d(/dir2|);
 d(/あ|);
-p(/portal|C:\test|rw|protected:file:|);
-f(/dir2/d0|C:\test.txt|r);
+p(/portal|C:\\test|rw|protected:file:|);
+f(/dir2/d0|C:\\test.txt|r);
         ";
 
     let parsed = parse_projection(projection.as_bytes()).unwrap();
